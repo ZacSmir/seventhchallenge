d7636fd425cbe17820ce19020c9a1fe5366559f5
ZacSmir <Zach@smirin.us>
2016-09-28 08:42:00 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var colorfulElements = $(".intro-section")
 var colorNameDisplay = $("#color-display")
-
+console.log("cats")
 colorfulElements.hover(function(e) {
   var elementHoveredOver = $(e.target)
   console.log(elementHoveredOver);
